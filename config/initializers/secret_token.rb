a4bd2ce57d61e5430d018c363ef1d2854de9f4bb
Yaniv <yanivlevi1@gmail.com>
2011-02-02 22:26:21 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tiviti::Application.config.secret_token = 'fe4709d0aec551ec259be6a9044f7897123aad93131350775b23789c0352909d4e9cfc64df9755031e60e0a141a103d2e11a7d7d79b3de0121bcdf3847522e6f'
